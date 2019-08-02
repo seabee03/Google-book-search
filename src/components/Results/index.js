5034f8ceb10c4d9c87e4b363bb03e9c8e90813c2
seabee03 <darcus.prado@gmail.com>
2019-08-01 20:47:54 -0500

    added images to Results/index.js

--- src/components/Results/index.js
+++ src/components/Results/index.js
@@ -3,10 +3,16 @@ import React from "react";
 function Results(props) {
     return (
         <div>
-            {console.log(props)}
-            <h3>{props.title}</h3>
-            <h6>{props.author}</h6>
-            <p>{props.description}</p>
+           <div className="result-card">
+               <div className="result-image">
+                   <img src={props.image} className="book-image" alt={props.title}/>
+               </div>
+               <div className="book-information">
+                    <h3>{props.title}</h3>
+                    <h6>{props.author}</h6>
+                    <p>{props.description}</p>
+               </div>
+           </div>
         </div>
     );
 }
