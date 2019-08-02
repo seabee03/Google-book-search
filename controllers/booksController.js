7a90b1847a0371e4168208f504225f54c8c5b7d8
seabee03 <darcus.prado@gmail.com>
2019-08-01 19:56:11 -0500

    added const db to controllers/booksController.js

---
+++ controllers/booksController.js
@@ -0,0 +1,10 @@
+const db = require("../models");
+
+module.exports = {
+    findAll: function(req, res) {
+        db.Book
+            .find(req.query).sort({date: -1})
+            .then(data => res.json(data))
+            .catch(err => res.status(422).json(err));
+    }
+}
